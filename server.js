fa6e941700e38d8e1365a2dabe6f9f2e2fee843b
dalyd14 <dalyd14@gmail.com>
2021-03-12 18:00:54 -0500

    added get route for index.html

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const path = require('path')
 
 const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
@@ -23,6 +24,10 @@ mongoose.connect(MONGODB_URI, {
 
 // routes
 app.use(require("./routes/api.js"));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
