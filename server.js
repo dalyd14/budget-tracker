abdef22cee69c5316dcdde0f34c3c76dd037e316
dalyd14 <dalyd14@gmail.com>
2021-03-14 09:52:18 -0400

    trying to add osme forwarding to https

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
+if(process.env.NODE_ENV === 'production') {
+  app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https')
+      res.redirect(`https://${req.header('host')}${req.url}`)
+    else
+      next()
+  })
+}
+
 app.use(logger("dev"));
 
 app.use(compression());
